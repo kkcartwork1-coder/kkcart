6296d00dcd410a742e34f4327b7c21794bd24026
Abhijit Nayak <abhijitnayak275@gmail.com>
2026-06-06 14:27:02 +0530

    Update location api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -18,31 +18,13 @@
 // export default API;
 
 
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-});
-
-// Attach token automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
-});
-
-export default API;
-
 // import axios from "axios";
 
 // const API = axios.create({
-//   baseURL: "https://kkcart-backend.onrender.com/api",
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 // });
 
+// // Attach token automatically
 // API.interceptors.request.use((req) => {
 //   const token = localStorage.getItem("token");
 
@@ -54,3 +36,42 @@ export default API;
 // });
 
 // export default API;
+
+// // import axios from "axios";
+
+// // const API = axios.create({
+// //   baseURL: "https://kkcart-backend.onrender.com/api",
+// // });
+
+// // API.interceptors.request.use((req) => {
+// //   const token = localStorage.getItem("token");
+
+// //   if (token) {
+// //     req.headers.Authorization = `Bearer ${token}`;
+// //   }
+
+// //   return req;
+// // });
+
+// // export default API;
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://your-backend.onrender.com/api",
+  // timeout: 15000,
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  req.headers["Content-Type"] = "application/json";
+
+  return req;
+});
+
+export default API;
